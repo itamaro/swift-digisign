625e0407ce74fcd54af0f8edca5b5ac0cfa96e8b
Itamar Ostricher <itamarost@gmail.com>
2013-04-11 17:17:18 +0300

    trying to be heard

--- integ/middleware.py
+++ integ/middleware.py
@@ -1,13 +1,21 @@
+from webob import Response
+from swift.common.swob import Request
+from swift.common.utils import get_logger
+
 class SwiftIntegrityMiddleware(object):
     """Middleware doing virus scan for Swift."""
 
     def __init__(self, app, conf):
         # app is the final application
         self.app = app
+        self.logger = get_logger(conf, log_route='proxy-server')
 
     def __call__(self, env, start_response):
-        if env['REQUEST_METHOD'] == 'PUT':
-            env['X-Object-Meta-Foo'] = 'bar'
+        req = Request(env)
+        req.headers['x-object-meta-foo'] = 'bar'
+        print "weeeeeeeeee"
+        #if env['REQUEST_METHOD'] == 'PUT':
+        #    env['X-Object-Meta-Foo'] = 'bar'
         return self.app(env, start_response)
 
 
