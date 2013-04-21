8f38a5a9a33e9013e9828c5fd3593c3ee4af0f46
itamaro <itamarost@gmail.com>
2013-04-21 03:03:35 -0700

    Middleware in place with stubs for meta tags and content body manipulation

--- integ/middleware.py
+++ integ/middleware.py
@@ -1,5 +1,5 @@
-from webob import Response
-from swift.common.swob import Request
+from webob import Response, Request
+from swift.common.swob import Request as SwobReq
 from swift.common.utils import get_logger
 
 class SwiftIntegrityMiddleware(object):
@@ -11,10 +11,20 @@ class SwiftIntegrityMiddleware(object):
         self.logger = get_logger(conf, log_route='proxy-server')
 
     def __call__(self, env, start_response):
+        swobreq = SwobReq(env)
         req = Request(env)
-        req.headers['x-object-meta-foo'] = 'bar'
-        print "weeeeeeeeee"
-        #if env['REQUEST_METHOD'] == 'PUT':
+        #req.headers['x-object-meta-foo'] = 'bar'
+        print "weeeeeeeeee-18-04--1"
+        #print repr(env)
+        if env['REQUEST_METHOD'] == 'PUT':
+            #print 'type(env)', type(env)
+            #print 'type(req)', type(req)
+            #print 'type(req-method)', type(env['REQUEST_METHOD'])
+            #print 'read env(wsgi.input)', env['wsgi.input'].read()
+            print 'req.method', req.method
+            print 'req.body', req.body
+            swobreq.headers['x-object-meta-foobar'] = 'barfoo'
+            #swobreq.headers['x-object-meta-content'] = env['wsgi.input'].read()
         #    env['X-Object-Meta-Foo'] = 'bar'
         return self.app(env, start_response)
 
