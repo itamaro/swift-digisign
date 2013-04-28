ef648364e506b94b6bb5ccf4a70960c837666766
itamaro <itamarost@gmail.com>
2013-04-28 09:34:58 -0700

    finished implemeting verification on object GET. added keys DB by username instead of hardcoded keys. fixed various bugs in handling requests that are not for objects (e.g. container). introduced new bug (vuln. actually) that allows adversary to successfully tamper with objects by setting her own username in the metadata. that sucks.

--- integ/middleware.py
+++ integ/middleware.py
@@ -1,34 +1,65 @@
 from webob import Response, Request
+from swift.common.http import is_success
 import cPickle
 from Crypto.Hash import SHA
 from Crypto import Random
+from swift.common.wsgi import make_pre_authed_env, make_pre_authed_request
+
+g_SigMeta = 'X-Object-Meta-DSA-Signature'
+g_SigUserMeta = 'X-Object-Meta-Sig-User'
 
 class SwiftIntegrityMiddleware(object):
-    """Middleware doing virus scan for Swift."""
+    """Middleware doing digital signature calculation and verification for Swift."""
 
     def __init__(self, app, conf):
         # app is the final application
         self.app = app
-        with open(conf.get('dsa_key_file'), 'rb') as f:
-            self.dsa_key = cPickle.load(f)
+        with open(conf.get('keys_file'), 'rb') as f:
+            self.keys_dict = cPickle.load(f)
 
+    def get_private_key(self, tenant):
+        return self.keys_dict[tenant]
+
+    def get_public_key(self, tenant):
+        return self.get_private_key(tenant).publickey()
 
     def __call__(self, env, start_response):
         req = Request(env)
-        print "weeeeeeeeee-21-04--3"
+        path_parts = req.path.strip('/').split('/')
+        if len(path_parts) < 4:
+            # Not handling requests that don't touch objects
+            return self.app(env, start_response)
+        tenant = req.remote_user.split(',')[1]
+        print "weeeeeeeeee-28-04--3 (%s)" % (req.path)
         if req.method == 'PUT':
             # Calculate SHA1 digest of content body (160bit)
             hash = SHA.new(req.body).digest()
             # Calculate DSA signature using loaded keys
-            sig = self.dsa_key.sign(hash, Random.new().read(19))
+            sig = self.get_private_key(tenant).sign(hash, Random.new().read(19))
             # Store signature (as string) in object metadata
-            req.headers['x-object-meta-dsa-signature'] = str(sig)
-        if req.method == 'GET':
-            # Obtain DSA signature from metadata, if it exists, and verify the content of the object
-            if 'x-object-meta-dsa-signature' in req.headers:
-                sig_str = req.headers['x-object-meta-dsa-signature']
-                sig = tuple(map(long, sig_str[1:-1].split(', ')))
-                #self.dsa_key.verify(
+            if 'tamper=1' == req.query_string:
+                print 'Tempaer backdoor activated on PUT'
+                sig = (sig[0]+1, sig[1]-1)
+            req.headers[g_SigMeta] = str(sig)
+            req.headers[g_SigUserMeta] = tenant
+        if req.method == 'GET' and 'tamper=1' <> req.query_string:
+            # Get the requested object
+            resp = make_pre_authed_request(env, 'GET', req.path, agent='SigCheck',
+                                           swift_source='SC').get_response(self.app)
+            if is_success(resp.status_int):
+                verified = False
+                # Obtain DSA signature from metadata, if it exists, and verify the content of the object
+                if g_SigMeta in resp.headers and g_SigUserMeta in resp.headers:
+                    # Calculate SHA1 digest of content body (160bit)
+                    hash = SHA.new(resp.body).digest()
+                    sig_str = resp.headers[g_SigMeta]
+                    sig_user = resp.headers[g_SigUserMeta]
+                    sig = tuple(map(long, sig_str[1:-1].split(', ')))
+                    verified = self.get_public_key(sig_user).verify(hash, sig)
+                if not verified:
+                    return Response(status=403, content_type='text/plain',
+                                    body='Failed verifying digital signature on '
+                                         'requested object')(env, start_response)
         return self.app(env, start_response)
 
 
